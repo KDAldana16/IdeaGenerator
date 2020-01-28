d5385c5c791297acc653db6652ccb3cb0b5ffc42
Karla Aldana <karla.aldana@students.dominican.edu>
2020-01-28 14:45:00 -0800

    Initial Commit

---
+++ IdeaGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdeaGenerator
+//
+//  Created by Karla Aldana on 1/28/20.
+//  Copyright © 2020 Karla Aldana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
